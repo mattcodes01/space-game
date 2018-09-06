ed1dcc6dc02e6c68978b693909036e5dee487658
MattCodes <mattcodes01@gmail.com>
2018-09-05 19:40:33 -0700

    add Rock to GameState for testing

--- src/Game/GameState.ts
+++ src/Game/GameState.ts
@@ -1,8 +1,11 @@
 import Entity from './Entity';
 import Ship from './Entities/Ship';
+import Rock from './Entities/Rock';
+import { degToRad } from '../util/Math';
 
 export default class GameState {
   entities: Entity[];
+
   init() {
     this.entities = [];
     const ship = new Ship({
@@ -11,13 +14,27 @@ export default class GameState {
         y: 500
       },
       size: {
-        height: 50,
+        height: 40,
         width: 50
       },
-      angle: (7 * Math.PI) / 4,
+      angle: 0,
+      vel: 2,
+      color: '#fff'
+    });
+    const rock = new Rock({
+      pos: {
+        x: 1500,
+        y: 500
+      },
+      size: {
+        height: 50,
+        width: 100
+      },
+      angle: 45,
       vel: 1
     });
 
     this.entities.push(ship);
+    this.entities.push(rock);
   }
 }
